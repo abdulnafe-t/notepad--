1a2b4f6fbfb6e7b9c76fb28800df6105251bc2f7
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-06-02 23:49:00 +0200

    [fix] Fix select all not overwriting text properly

--- src/Edit.cpp
+++ src/Edit.cpp
@@ -50,8 +50,8 @@ void Edit::paste(File_io& file) {
 };
 
 void Edit::select_all(File_io& file) {
-      file.set_mark(0);
       file.move(static_cast<int>(file.get_last_char_index()) + 1);
+      file.set_mark(0);
       GUI::cursor.set_row(file.get_number_of_lines());
       GUI::cursor.set_column(file.get_line_size(file.get_cursor_position()));
 };
