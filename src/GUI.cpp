cd8e5c71048dca69b115a33feea4e6707db17bd6
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-06-03 18:21:01 +0200

    [minor] Adjust font size

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,7 +1,7 @@
 #include "GUI.h"
 
 namespace GUI {
-      int   font_size {20};
+      int   font_size {21};
       float font_size_px {static_cast<float>(font_size * 96.0 / 72.0)};
       float main_menu_padding {4.};
 
